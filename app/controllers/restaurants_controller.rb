418beb96011d26668bd77ec57337b7a6fa0d370f
Kieran Lambe <lambekieran@me.com>
2024-02-08 20:53:23 +0000

    the arrow of time moves ever onwards

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,20 @@
 class RestaurantsController < ApplicationController
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+  end
+
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :rating, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @review.save
+  end
+
   def new
+    @reivew = Review.new
+  end
+
+  def index
+    @reviews = Review.all
+  end
+  
+  private
+
+  def review_params
+    params.require(:review).permit(:restaurant_id, :content, :rating)
   end
 end
